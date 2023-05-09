9d1aa8fcc76c2e73242e6bbe3cb0d1417159bc20
agonzalezrando <93986162+AlvaroCesur@users.noreply.github.com>
2023-05-09 16:02:54 +0200

    Commit dia 9/5

--- bot.py
+++ bot.py
@@ -1,13 +1,26 @@
 import os
 import telebot
+# import requests
+# import json
 
 BOT_TOKEN = os.getenv('TOKEN')
-print(BOT_TOKEN)
 bot = telebot.TeleBot(BOT_TOKEN)
+# url = "https://api.telegram.org/" + BOT_TOKEN + "/getFile"
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    fileID = message.photo[-1].file_id
+    print (message)
+    print("------------")
+    print(fileID)
+    file_info = bot.get_file(fileID)
+    bot.reply_to (message, 'file.file_path =' + file_info.file_path)
+    # Taggear las fotos con las imagenes
 
 # Function to handle messages
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func = lambda message: True)
 def handle_message(message):
+    print(message)
     # Check if the message is a command
     if message.text.startswith('/'):
         handle_command(message)
@@ -18,6 +31,8 @@ def handle_message(message):
 def handle_command(message):
     command = message.text[1:].lower()
     if command == 'start':
-        bot.reply_to(message, "Vale, vamos a empezar.")
+        bot.reply_to(message, "¿Quieres crear un nuevo viaje o gestionar uno ya existente?")
+        if command == 'nuevo':
+            bot.reply_to(message, "¿Cómo quieres llamar al viaje?")
 
 bot.polling()
